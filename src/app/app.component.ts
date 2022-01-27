27972bd1acb2d316d510706c0f2fdef1343a9b95
pranavgvrk <pranavgvrk@hotmail.com>
2022-01-28 00:46:50 +0530

    4 files modified, 3 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>My App</h1>',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   name = 'Angular ' + VERSION.major;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 // import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MediaItemComponent } from './media-item.component';
 // import { HelloComponent } from './hello.component';
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, MediaItemComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
